cbca9b781bffe3ed92fdf3546722238932241447
Syres916 <46537884+Syres916@users.noreply.github.com>
2024-05-04 20:07:19 +0100

    [

--- src/Mod/Draft/draftguitools/gui_grid.py
+++ src/Mod/Draft/draftguitools/gui_grid.py
@@ -56,14 +56,18 @@ class ToggleGrid(gui_base.GuiCommandSimplest):
     def GetResources(self):
         """Set icon, menu and tooltip."""
 
-        return {"Pixmap": "Draft_Grid",
-                "Accel": "G, R",
-                "MenuText": QT_TRANSLATE_NOOP("Draft_ToggleGrid", "Toggle grid"),
-                "ToolTip": QT_TRANSLATE_NOOP("Draft_ToggleGrid",
-                                             "Toggles the Draft grid on and off."),
-                "CmdType": "ForEdit"}
+        return {
+            "Pixmap": "Draft_Grid",
+            "Accel": "G, R",
+            "CmdType": "ForEdit",
+            "MenuText": QT_TRANSLATE_NOOP("Draft_ToggleGrid", "Toggle grid"),
+            "ToolTip": QT_TRANSLATE_NOOP(
+                "Draft_ToggleGrid", "Toggles the Draft grid on and off."
+            ),
+            'Checkable': False,
+        }
 
-    def Activated(self):
+    def Activated(self, index = 0):
         """Execute when the command is called."""
         super().Activated()
 
