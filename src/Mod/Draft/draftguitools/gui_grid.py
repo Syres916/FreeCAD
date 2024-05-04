0aaec329210463be2f7a9e4acddbbff0d9205dfe
Syres916 <46537884+Syres916@users.noreply.github.com>
2024-05-04 15:48:16 +0100

    [Draft] Add grid observer

--- src/Mod/Draft/draftguitools/gui_grid.py
+++ src/Mod/Draft/draftguitools/gui_grid.py
@@ -36,6 +36,7 @@ import FreeCADGui as Gui
 import WorkingPlane
 
 from draftguitools import gui_base
+from draftutils import grid_observer
 from draftutils.translate import translate
 
 
@@ -76,14 +77,17 @@ class ToggleGrid(gui_base.GuiCommandSimplest):
         if grid.Visible:
             grid.off()
             grid.show_always = False
+            grid_observer._update_gridgui()
             if cmdactive:
                 grid.show_during_command = False
         elif cmdactive:
             grid.set()  # set() required: the grid must be updated to match the current WP
             grid.show_during_command = True
+            grid_observer._update_gridgui()
         else:
             grid.set()
             WorkingPlane.get_working_plane()
+            grid_observer._update_gridgui()
             grid.show_always = True
 
 Gui.addCommand("Draft_ToggleGrid", ToggleGrid())
