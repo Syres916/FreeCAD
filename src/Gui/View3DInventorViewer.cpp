6af86be4dc3ec30f74ca18bcb21b5b71e50dad54
Syres916 <46537884+Syres916@users.noreply.github.com>
2025-02-03 13:40:16 +0000

    [Gui] correct variables to more meaningful names

--- src/Gui/View3DInventorViewer.cpp
+++ src/Gui/View3DInventorViewer.cpp
@@ -2451,24 +2451,24 @@ void View3DInventorViewer::renderScene()
             ("User parameter:BaseApp/Preferences/View");
         unsigned long axisLetterColor = hGrpView->GetUnsigned("AxisLetterColor", 4294902015); //default FPS color (yellow)
         QColor color = App::Color::fromPackedRGBA<QColor>(axisLetterColor);
-        double ucRed = float(color.redF());
-        double ucGreen = float(color.greenF());
-        double ucBlue = float(color.blueF());
+        double fColRed = float(color.redF());
+        double fColGreen = float(color.greenF());
+        double fColBlue = float(color.blueF());
         if (overlayLeftWidgets == "") {
             draw2DString(stream.str().c_str(),
                          SbVec2s(10, 10),
                          SbVec2f(0.1F, 0.1F),
-                         ucRed,
-                         ucGreen,
-                         ucBlue);  // NOLINT
+                         fColRed,
+                         fColGreen,
+                         fColBlue);  // NOLINT
         }
         else {
             draw2DString(stream.str().c_str(),
                          SbVec2s(10, 10),
                          SbVec2f(1.1F, 0.1F),
-                         ucRed,
-                         ucGreen,
-                         ucBlue);  // NOLINT
+                         fColRed,
+                         fColGreen,
+                         fColBlue);  // NOLINT
         }
     }
 
