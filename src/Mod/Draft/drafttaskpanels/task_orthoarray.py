d8ddc0b3c2930d6fac7c026a8d36010e13d62a3f
Syres916 <46537884+Syres916@users.noreply.github.com>
2025-06-10 00:54:20 +0100

    Draft and Path checkbox fixes

--- src/Mod/Draft/drafttaskpanels/task_orthoarray.py
+++ src/Mod/Draft/drafttaskpanels/task_orthoarray.py
@@ -164,8 +164,12 @@ class TaskPanelOrthoArray:
         self.form.button_reset_Z.clicked.connect(lambda: self.reset_v("Z"))
 
         # When the checkbox changes, change the internal value
-        self.form.checkbox_fuse.stateChanged.connect(self.set_fuse)
-        self.form.checkbox_link.stateChanged.connect(self.set_link)
+        if hasattr(self.form.checkbox_fuse, "checkStateChanged"):
+            self.form.checkbox_fuse.checkStateChanged.connect(self.set_fuse)
+            self.form.checkbox_link.checkStateChanged.connect(self.set_link)
+        else:
+            self.form.checkbox_fuse.stateChanged.connect(self.set_fuse)
+            self.form.checkbox_link.stateChanged.connect(self.set_link)
 
 
     def accept(self):
