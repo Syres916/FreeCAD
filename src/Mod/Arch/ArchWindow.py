cbb0eb848d5311cebb33311a01ec8910692ae5d6
Syres916 <46537884+Syres916@users.noreply.github.com>
2025-07-03 09:12:51 +0100

    [Arch] Fix bug in QtSvgWidgets import

--- src/Mod/Arch/ArchWindow.py
+++ src/Mod/Arch/ArchWindow.py
@@ -363,9 +363,9 @@ class _CommandWindow:
         "sets up a taskbox widget"
         import PySide
         if PySide.__version_info__[0] == 6:
-            from PySide6 import QtSvgWidgets
-        else:
             from PySide import QtSvgWidgets
+        else:
+            from PySide import QtSvg as QtSvgWidgets
         from PySide import QtCore, QtGui
         from ArchWindowPresets import WindowPresets
         w = QtGui.QWidget()
