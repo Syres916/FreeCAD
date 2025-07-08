7d081b70d01aeb9ead30448c8d18a90b88c6078a
Syres916 <46537884+Syres916@users.noreply.github.com>
2025-07-08 19:56:40 +0100

    [TechDraw] change arrow text for icon and adjust width of dialog

--- src/Mod/TechDraw/TechDrawTools/TaskFillTemplateFields.py
+++ src/Mod/TechDraw/TechDrawTools/TaskFillTemplateFields.py
@@ -140,9 +140,10 @@ class TaskFillTemplateFields:
                 updateCb = QtCore.QT_TRANSLATE_NOOP(
                     "TechDraw_FillTemplateFields", "Update"
                 )
-                updateTxt = (
-                    "                                                       ==>>"
-                )
+                icon_name = "button_right"
+                svg = ":/icons/" + icon_name
+                pix = QtGui.QPixmap(svg)
+                icon = QtGui.QIcon.fromTheme(icon_name, QtGui.QIcon(svg))
                 self.checkBoxList = []
                 self.lineTextList = []
                 dialogRow = 0
@@ -158,7 +159,8 @@ class TaskFillTemplateFields:
                         else:
                             self.cb1.setChecked(QtCore.Qt.Checked)
                         self.la.addWidget(self.cb1, dialogRow, 1)
-                        u1 = QtGui.QLabel(updateTxt)
+                        u1 = QtGui.QLabel("")
+                        u1.setPixmap(pix.scaled(32, 32))
                         self.la.addWidget(u1, dialogRow, 2)
                         self.s1 = QtGui.QLineEdit()
                         self.s1.setText(App.ActiveDocument.CreatedBy)
@@ -179,7 +181,8 @@ class TaskFillTemplateFields:
                         else:
                             self.cb2.setChecked(QtCore.Qt.Checked)
                         self.la.addWidget(self.cb2, dialogRow, 1)
-                        u2 = QtGui.QLabel(updateTxt)
+                        u2 = QtGui.QLabel("")
+                        u2.setPixmap(pix.scaled(32, 32))
                         self.la.addWidget(u2, dialogRow, 2)
                         self.s2 = QtGui.QLineEdit()
                         self.la.addWidget(self.s2, dialogRow, 3)
@@ -203,7 +206,8 @@ class TaskFillTemplateFields:
                         else:
                             self.cb3.setChecked(QtCore.Qt.Checked)
                         self.la.addWidget(self.cb3, dialogRow, 1)
-                        u3 = QtGui.QLabel(updateTxt)
+                        u3 = QtGui.QLabel("")
+                        u3.setPixmap(pix.scaled(32, 32))
                         self.la.addWidget(u3, dialogRow, 2)
                         self.s3 = QtGui.QLineEdit()
                         self.la.addWidget(self.s3, dialogRow, 3)
@@ -224,7 +228,8 @@ class TaskFillTemplateFields:
                         else:
                             self.cb4.setChecked(QtCore.Qt.Checked)
                         self.la.addWidget(self.cb4, dialogRow, 1)
-                        u4 = QtGui.QLabel(updateTxt)
+                        u4 = QtGui.QLabel("")
+                        u4.setPixmap(pix.scaled(32, 32))
                         self.la.addWidget(u4, dialogRow, 2)
                         self.s4 = QtGui.QLineEdit()
                         self.la.addWidget(self.s4, dialogRow, 3)
@@ -234,7 +239,7 @@ class TaskFillTemplateFields:
                         self.checkBoxList.append(self.cb4)
                         self.lineTextList.append(self.s4)
                         self.cb4.clicked.connect(self.on_cb4_clicked)
-                        dialog += 1
+                        dialogRow += 1
                     if str(key).lower() in CompanyChkLst:
                         t5 = QtGui.QLabel(value)
                         self.la.addWidget(t5, dialogRow, 0)
@@ -245,7 +250,8 @@ class TaskFillTemplateFields:
                         else:
                             self.cb5.setChecked(QtCore.Qt.Checked)
                         self.la.addWidget(self.cb5, dialogRow, 1)
-                        u5 = QtGui.QLabel(updateTxt)
+                        u5 = QtGui.QLabel("")
+                        u5.setPixmap(pix.scaled(32, 32))
                         self.la.addWidget(u5, dialogRow, 2)
                         self.s5 = QtGui.QLineEdit()
                         self.la.addWidget(self.s5, dialogRow, 3)
@@ -266,7 +272,8 @@ class TaskFillTemplateFields:
                         else:
                             self.cb6.setChecked(QtCore.Qt.Checked)
                         self.la.addWidget(self.cb6, dialogRow, 1)
-                        u6 = QtGui.QLabel(updateTxt)
+                        u6 = QtGui.QLabel("")
+                        u6.setPixmap(pix.scaled(32, 32))
                         self.la.addWidget(u6, dialogRow, 2)
                         self.s6 = QtGui.QLineEdit()
                         self.la.addWidget(self.s6, dialogRow, 3)
@@ -287,7 +294,8 @@ class TaskFillTemplateFields:
                         else:
                             self.cb7.setChecked(QtCore.Qt.Checked)
                         self.la.addWidget(self.cb7, dialogRow, 1)
-                        u7 = QtGui.QLabel(updateTxt)
+                        u7 = QtGui.QLabel("")
+                        u7.setPixmap(pix.scaled(32, 32))
                         self.la.addWidget(u7, dialogRow, 2)
                         self.s7 = QtGui.QLineEdit()
                         self.la.addWidget(self.s7, dialogRow, 3)
@@ -337,7 +345,8 @@ class TaskFillTemplateFields:
                         else:
                             self.cb8.setChecked(QtCore.Qt.Checked)
                         self.la.addWidget(self.cb8, dialogRow, 1)
-                        u8 = QtGui.QLabel(updateTxt)
+                        u8 = QtGui.QLabel("")
+                        u8.setPixmap(pix.scaled(32, 32))
                         self.la.addWidget(u8, dialogRow, 2)
                         self.s8 = QtGui.QLineEdit()
                         self.la.addWidget(self.s8, dialogRow, 3)
@@ -404,9 +413,11 @@ class TaskFillTemplateFields:
                     QtCore.QObject.connect(
                         self.okbox, QtCore.SIGNAL("rejected()"), self.close
                     )
+                    self.okbox.button(QtGui.QDialogButtonBox.Ok).setText("&Ok")
+                    self.okbox.button(QtGui.QDialogButtonBox.Cancel).setText("&Cancel")
                     self.button = self.okbox.button(QtGui.QDialogButtonBox.Ok)
                     self.button.setEnabled(True)
-                    self.dialog.resize(1050, dialogRow * 50 + 75)
+                    self.dialog.resize(700, dialogRow * 50 + 75)
                     self.dialog.move(400, 200 * (400 / (dialogRow * 50 + 75)))
                     QtCore.QMetaObject.connectSlotsByName(self.dialog)
                     self.dialog.show()
