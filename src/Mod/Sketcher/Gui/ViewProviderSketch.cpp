6dfc56b773ae9efa5cac177c67e9cef6612039c0
Syres916 <46537884+Syres916@users.noreply.github.com>
2025-11-28 10:35:46 +0000

    [Sketcher] make sure a sketch in an empty doc does not run ViewFit

--- src/Mod/Sketcher/Gui/ViewProviderSketch.cpp
+++ src/Mod/Sketcher/Gui/ViewProviderSketch.cpp
@@ -3663,9 +3663,11 @@ void ViewProviderSketch::setEditViewer(Gui::View3DInventorViewer* viewer, int Mo
     cameraSensor.setData(new VPRender {this, viewer->getSoRenderManager()});
     cameraSensor.attach(viewer->getSoRenderManager()->getSceneGraph());
 
-    QString cmdstr = QString::fromLatin1("Gui.SendMsgToActiveView('ViewFit')\n");
-    QByteArray cmdstr_bytearray = cmdstr.toLatin1();
-    Gui::Command::runCommand(Gui::Command::Gui, cmdstr_bytearray);
+    if (dist != 100.000000) {
+        QString cmdstr = QString::fromLatin1("Gui.SendMsgToActiveView('ViewFit')\n");
+        QByteArray cmdstr_bytearray = cmdstr.toLatin1();
+        Gui::Command::runCommand(Gui::Command::Gui, cmdstr_bytearray);
+    }
 }
 
 void ViewProviderSketch::unsetEditViewer(Gui::View3DInventorViewer* viewer)
