787626b83ba4b0ab654aec284096061b85533644
Syres916 <46537884+Syres916@users.noreply.github.com>
2025-12-08 11:08:52 +0000

    [Sketcher] change setRubberBand to return dist

--- src/Mod/Sketcher/Gui/ViewProviderSketch.h
+++ src/Mod/Sketcher/Gui/ViewProviderSketch.h
@@ -660,7 +660,7 @@ protected:
     void unsetEdit(int ModNum) override;
     void setEditViewer(Gui::View3DInventorViewer*, int ModNum) override;
     void unsetEditViewer(Gui::View3DInventorViewer*) override;
-    void setRubberBand(Gui::View3DInventorViewer* viewer, SoCamera* cam);
+    double setRubberBand(Gui::View3DInventorViewer* viewer, SoCamera* cam, Base::Placement plm, SbRotation rot);
     static void camSensCB(void* data, SoSensor*);// camera sensor callback
     void onCameraChanged(SoCamera* cam);
     //@}
