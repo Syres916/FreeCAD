1d4b14e6246e20a59562747db8b559325a2f2d1f
Syres916 <46537884+Syres916@users.noreply.github.com>
2026-03-28 15:06:48 +0000

    [TechDraw] Replace qAsConst with std::as_const

--- src/Mod/TechDraw/Gui/MDIViewPage.cpp
+++ src/Mod/TechDraw/Gui/MDIViewPage.cpp
@@ -954,7 +954,7 @@ void MDIViewPage::sceneSelectionManager()
     //add to m_qgSceneSelected anything that is in q_sceneSel
     for (auto qts : sceneSel) {
         bool found = false;
-        for (auto ms : qAsConst(m_qgSceneSelected)) {
+        for (auto ms : std::as_const(m_qgSceneSelected)) {
             if (qts == ms) {
                 found = true;
                 break;
@@ -968,7 +968,7 @@ void MDIViewPage::sceneSelectionManager()
 
     //remove items from m_qgSceneSelected that are not in q_sceneSel
     QList<QGraphicsItem*> m_new;
-    for (auto m : qAsConst(m_qgSceneSelected)) {
+    for (auto m : std::as_const(m_qgSceneSelected)) {
         for (auto q : sceneSel) {
             if (m == q) {
                 m_new.push_back(m);
